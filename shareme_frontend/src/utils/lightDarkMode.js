dc6341d5505be2c763476e9272f3589698ef7162
Cole Winchester <cowinch@gmail.com>
2022-10-21 00:32:20 -0500

    light-dark-mode concept

---
+++ shareme_frontend/src/utils/lightDarkMode.js
@@ -0,0 +1,17 @@
+
+//main background
+const mainLightBg = "bg-gray-50"
+const mainDarkBg = "bg-gray-800"
+
+//main text
+const mainLightText =''
+const mainDarkText=''
+
+
+//sidebar background
+const sideLightBg="bg-white"
+const sideDarkBg="bg-black"
+
+
+let sideBg = sideDarkBg
+let mainBg =mainDarkBg
